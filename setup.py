01b7093c6d7e52df9a623e40da5d7803f20de1ca
Samuel Marks <samuelmarks@gmail.com>
2015-02-18 18:40:57 +1100

    Experimenting with different setuptools arguments

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ if __name__ == '__main__':
     package_name = 'bettertutors_user_api'
     setup(
         name=package_name,
-        version='0.2.10',
+        version='0.2.11',
         author='Samuel Marks',
         py_modules=[package_name],
         test_suite='tests',
         install_requires=[
             'bottle', 'webtest'
-        ]
+        ],
+        dependency_links=['https://github.com/bettertutors/sql-models/tarball/master#egg=sql-models']
     )
